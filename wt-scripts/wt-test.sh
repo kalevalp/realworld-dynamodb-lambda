95492f113e42b34bf472500d75253aad076ea476
Kalev Alpernas <kalev.alp@gmail.com>
2020-09-11 00:17:33 +0300

    test script changes (for interleavings report)

--- wt-scripts/wt-test.sh
+++ wt-scripts/wt-test.sh
@@ -4,29 +4,29 @@ export resdir=results`date +%Y%m%d%H%M%S`
 mkdir ${resdir}
 
 function run {
-    serverless deploy
-    # sleep 10
     # serverless deploy
     # sleep 10
     # serverless deploy
-    sleep 60
+    # sleep 10
+    # serverless deploy
+    sleep 20
 
-    for j in $(seq 1 5)
-    do
-	for i in $(seq 1 5)
+    # for j in $(seq 1 5)
+    # do
+	for i in $(seq 1 2)
 	do
 	    npm run test:deployed
 	    sleep 10
 	done
-	echo Iteration ${j} &>> ${curr_resdir}/ddb-usage
-	node wt-scripts/analyze-ddb-use.js &>> ${curr_resdir}/ddb-usage
-    done
+	# echo Iteration ${j} &>> ${curr_resdir}/ddb-usage
+	# node wt-scripts/analyze-ddb-use.js &>> ${curr_resdir}/ddb-usage
+    # done
 
-    node wt-scripts/get-wt-times.js ${curr_resdir}/
+    # node wt-scripts/get-wt-times.js ${curr_resdir}/
 
     sleep 60
 
-    serverless remove
+    # serverless remove
 }
 
 # export WT_RUN_NO_RECORDING=''
@@ -42,13 +42,20 @@ function run {
 # do
 
 export pcount=9
-export curr_resdir=${resdir}/rec-res-${pcount}-props
-mkdir ${curr_resdir}
-
 export WT_RW_PROP_COUNT=${pcount}
+for parrun in $(seq 1 5)
+do
+    export curr_resdir=${resdir}/rec-res-${parrun} #-${pcount}-props
+    mkdir ${curr_resdir}
 
-run &> ${curr_resdir}/run-output
 
-unset WT_RW_PROP_COUNT
-unset curr_resdir
+    run &> ${curr_resdir}/run-output &
+    sleep 0.5
+
+    unset curr_resdir
+
+done
+
+# unset WT_RW_PROP_COUNT
+# unset curr_resdir
 # done
